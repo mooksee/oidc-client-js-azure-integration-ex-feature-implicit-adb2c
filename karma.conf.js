8078b851ea9b7635d57897285f65b692e776f2f4
mooksee <ouamar@outlook.fr>
2024-04-25 15:55:34 +0200

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -3,15 +3,23 @@
 
 module.exports = function (config) {
   config.set({
-    basePath: '',
-    frameworks: ['jasmine', '@angular-devkit/build-angular'],
-    plugins: [
-      require('karma-jasmine'),
-      require('karma-chrome-launcher'),
-      require('karma-jasmine-html-reporter'),
-      require('karma-coverage-istanbul-reporter'),
-      require('@angular-devkit/build-angular/plugins/karma')
-    ],
+    // ... other configuration options
+
+    browsers: ['ChromeHeadless'],
+
+    customLaunchers: {
+      ChromeHeadless: {
+        base: 'Chrome',
+        flags: [
+          '--no-sandbox',
+          '--headless',
+          '--disable-gpu',
+          '--remote-debugging-port=9222',
+        ],
+      },
+    },
+  });
+};    
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
